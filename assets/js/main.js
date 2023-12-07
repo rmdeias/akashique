f92774061e98d65c64538842237aa1d692e24666
rdeias <rdeias@mediastay.com>
2023-12-07 19:14:35 +0100

    fix css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,53 @@
-$(".form").submit(function(e){
-    e.preventDefault();
-});
+
+var logo = document.getElementById("akashiques-medium-guidance-energeticienne-banner");
+var nav = document.getElementById("akashiques-medium-guidance-energeticienne-navDesktop");
+var main = document.getElementsByTagName("main");
+const isFirefox = typeof InstallTrigger !== 'undefined';
+
+// When the user scrolls down from the top of the document logohidden and navabar fix to the top
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+
+    if(window.screen.width >= 768){
+
+        nav.classList.remove("sticky");
+
+        logo.style.display = "flex";
+        main[0].classList.remove("marginTopMain");
+
+        if (document.body.scrollTop >= logo.offsetHeight || document.documentElement.scrollTop >= logo.offsetHeight ) {
+
+            logo.style.display = "none";
+            nav.classList.add("sticky");
+            main[0].classList.add("marginTopMain");
+        }
+
+    }
+}
+
+//fix anchoring offset with nav
+$(document).ready( function () {
+   $('#akashiques-medium-guidance-energeticienne-navDesktop li').click(function() {
+       console.log( $(this).children().attr('href'))
+       var link = $(this).children().attr('href')
+     $('html,body').animate({scrollTop: $(link).offset().top- 150}, 'slow' );
+   });
+})
+
+
+//for hide or show #akashiques-medium-guidance-energeticienne-navDesktop2
+$("#prestationNav").mouseover(function() {
+        $("#akashiques-medium-guidance-energeticienne-navDesktop2" ).css("display" , "block" );
+    }
+);
+$(".navLinks").mouseover(function() {
+        $("#akashiques-medium-guidance-energeticienne-navDesktop2" ).css("display" , "none" );
+
+    }
+);
+$("#prestationNav").click(function() {
+        $("#akashiques-medium-guidance-energeticienne-navDesktop2" ).css("display" , "none" );
+
+    }
+);
+
